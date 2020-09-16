9a9e635aa646ab60b50455d962151ec317f3425b
Doris8462 <liandou123>
2020-09-16 16:50:17 +0800

    [haungyunjie] feat:method 1

--- src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingController.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingController.java
@@ -1,16 +1,20 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Scope(value = "prototype")
 public class GreetingController {
 
     private final GreetingService greetingService;
 
     @Autowired
     public GreetingController(GreetingService greetingService) {
+        System.out.println("+++++++GreetingController is instantiating...");
+
         this.greetingService = greetingService;
     }
 
--- src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingService.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingService.java
@@ -1,9 +1,14 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
 @Service
+@Scope(value = "prototype")
 public class GreetingService {
+    public GreetingService(){
+        System.out.println("+++++++GreetingService is instantiating...");
+    }
 
     String sayHi() {
         return "hello world";
